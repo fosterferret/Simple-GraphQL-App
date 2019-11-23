1ebfaf4b822e641d71f97105a52629016efc09fd
Francis Bulus <notchera@gmail.com>
2019-11-23 16:43:45 +0100

    building out first resource in schema

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const app = express();
 
+app.use("/graphql", graphqlHTTP({}));
+
 app.listen(5000, () => {
   console.log(`Listening for requests on port ${400}`);
 });
