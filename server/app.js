8ce1d578a936419e35e780bc8ee04abe2f531d3e
Francis Bulus <notchera@gmail.com>
2019-11-23 23:53:13 +0100

    created mongoose models for app

--- server/app.js
+++ server/app.js
@@ -4,6 +4,21 @@ const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const app = express();
 
+async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://francis:madman@cluster0-03hwy.mongodb.net/",
+      { dbName: "graphql" }
+    );
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+mongoose.connection.once("open", () => {
+  console.log("motherfucker dhdhhdhdhd");
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
